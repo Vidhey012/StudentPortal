fc0606e9c935cdfe0854d73ec765797324c4627e
Vidhey012 <vidhey.bhogadi2003@gmail.com>
2021-02-01 22:13:25 +0530

    Student Portal Second Commit

---
+++ src/java/com/spring/Model/Register.java
@@ -0,0 +1,372 @@
+package com.spring.Model;
+
+import java.sql.Date;
+
+public class Register {
+
+	private Integer id;
+	private Integer studentid;
+	private Integer reasonid;
+	private Integer detailid;
+	private Integer qid;
+	private Integer semid;
+	private Integer facultyattendanceid;
+	private Integer studentattendanceid;
+	private Integer studentmarksid;
+	private Integer branchid;
+	private Integer faqid;
+	private Integer mid1;
+	private Integer mid2;
+	private Integer mid3;
+	private Integer subjectid;
+	private String username;
+	private String password;
+	private String name;
+	private String qname;
+	private String email;
+	private String mobileno;
+	private String branchcode;
+	private String oldbranchcode;
+	private String role;
+	private String qualification;
+	private String status;
+	private String facultyid;
+	private String stu_pin1;
+	private String stu_pin2;
+	private String stu_pin3;
+	private String sem;
+	private String subject;
+	private String feedback;
+	private String reply;
+	private String gender;
+	private String experience;
+	private String address;
+	private String oldpassword;
+	private String newpassword;
+	private String oldemail;
+	private String question;
+	private String answer;
+	private String attendance;
+	private String reason;
+	private String detailname;
+	private String detailvalue;
+	private String maplink;
+	private String year;
+	private Date date;
+	private Date sdate;
+	private Date edate;
+	
+	public String getOldemail() {
+		return oldemail;
+	}
+	public void setOldemail(String oldemail) {
+		this.oldemail = oldemail;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getExperience() {
+		return experience;
+	}
+	public void setExperience(String experience) {
+		this.experience = experience;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getSem() {
+		return sem;
+	}
+	public void setSem(String sem) {
+		this.sem = sem;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public Integer getStudentattendanceid() {
+		return studentattendanceid;
+	}
+	public void setStudentattendanceid(Integer studentattendanceid) {
+		this.studentattendanceid = studentattendanceid;
+	}
+	public String getStu_pin1() {
+		return stu_pin1;
+	}
+	public void setStu_pin1(String stu_pin1) {
+		this.stu_pin1 = stu_pin1;
+	}
+	public String getStu_pin2() {
+		return stu_pin2;
+	}
+	public void setStu_pin2(String stu_pin2) {
+		this.stu_pin2 = stu_pin2;
+	}
+	public String getStu_pin3() {
+		return stu_pin3;
+	}
+	public void setStu_pin3(String stu_pin3) {
+		this.stu_pin3 = stu_pin3;
+	}
+	public String getFacultyid() {
+		return facultyid;
+	}
+	public void setFacultyid(String facultyid) {
+		this.facultyid = facultyid;
+	}
+	public Integer getFacultyattendanceid() {
+		return facultyattendanceid;
+	}
+	public void setFacultyattendanceid(Integer facultyattendanceid) {
+		this.facultyattendanceid = facultyattendanceid;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getQualification() {
+		return qualification;
+	}
+	public void setQualification(String qualification) {
+		this.qualification = qualification;
+	}
+	public Integer getBranchid() {
+		return branchid;
+	}
+	public void setBranchid(Integer branchid) {
+		this.branchid = branchid;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getMobileno() {
+		return mobileno;
+	}
+	public void setMobileno(String mobileno) {
+		this.mobileno = mobileno;
+	}
+	public String getBranchcode() {
+		return branchcode;
+	}
+	public void setBranchcode(String branchcode) {
+		this.branchcode = branchcode;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public String getOldbranchcode() {
+		return oldbranchcode;
+	}
+	public void setOldbranchcode(String oldbranchcode) {
+		this.oldbranchcode = oldbranchcode;
+	}
+	public Integer getMid1() {
+		return mid1;
+	}
+	public void setMid1(Integer mid1) {
+		this.mid1 = mid1;
+	}
+	public Integer getMid2() {
+		return mid2;
+	}
+	public void setMid2(Integer mid2) {
+		this.mid2 = mid2;
+	}
+	public Integer getMid3() {
+		return mid3;
+	}
+	public void setMid3(Integer mid3) {
+		this.mid3 = mid3;
+	}
+	public Integer getStudentmarksid() {
+		return studentmarksid;
+	}
+	public void setStudentmarksid(Integer studentmarksid) {
+		this.studentmarksid = studentmarksid;
+	}
+	public String getFeedback() {
+		return feedback;
+	}
+	public void setFeedback(String feedback) {
+		this.feedback = feedback;
+	}
+	public String getReply() {
+		return reply;
+	}
+	public void setReply(String reply) {
+		this.reply = reply;
+	}
+	public String getOldpassword() {
+		return oldpassword;
+	}
+	public void setOldpassword(String oldpassword) {
+		this.oldpassword = oldpassword;
+	}
+	public String getNewpassword() {
+		return newpassword;
+	}
+	public void setNewpassword(String newpassword) {
+		this.newpassword = newpassword;
+	}
+	public Integer getSubjectid() {
+		return subjectid;
+	}
+	public void setSubjectid(Integer subjectid) {
+		this.subjectid = subjectid;
+	}
+	public Integer getSemid() {
+		return semid;
+	}
+	public void setSemid(Integer semid) {
+		this.semid = semid;
+	}
+	
+	public Integer getQid() {
+		return qid;
+	}
+	public void setQid(Integer qid) {
+		this.qid = qid;
+	}
+	public String getQname() {
+		return qname;
+	}
+	public void setQname(String qname) {
+		this.qname = qname;
+	}
+	public Integer getFaqid() {
+		return faqid;
+	}
+	public void setFaqid(Integer faqid) {
+		this.faqid = faqid;
+	}
+	public String getQuestion() {
+		return question;
+	}
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+	public String getAnswer() {
+		return answer;
+	}
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+	public String getAttendance() {
+		return attendance;
+	}
+	public void setAttendance(String attendance) {
+		this.attendance = attendance;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public Date getSdate() {
+		return sdate;
+	}
+	public void setSdate(Date sdate) {
+		this.sdate = sdate;
+	}
+	public Date getEdate() {
+		return edate;
+	}
+	public void setEdate(Date edate) {
+		this.edate = edate;
+	}
+	public String getYear() {
+		return year;
+	}
+	public void setYear(String year) {
+		this.year = year;
+	}
+	public Integer getStudentid() {
+		return studentid;
+	}
+	public void setStudentid(Integer studentid) {
+		this.studentid = studentid;
+	}
+	public Integer getReasonid() {
+		return reasonid;
+	}
+	public void setReasonid(Integer reasonid) {
+		this.reasonid = reasonid;
+	}
+	public String getReason() {
+		return reason;
+	}
+	public void setReason(String reason) {
+		this.reason = reason;
+	}
+	public Integer getDetailid() {
+		return detailid;
+	}
+	public void setDetailid(Integer detailid) {
+		this.detailid = detailid;
+	}
+	public String getDetailname() {
+		return detailname;
+	}
+	public void setDetailname(String detailname) {
+		this.detailname = detailname;
+	}
+	public String getDetailvalue() {
+		return detailvalue;
+	}
+	public void setDetailvalue(String detailvalue) {
+		this.detailvalue = detailvalue;
+	}
+	public String getMaplink() {
+		return maplink;
+	}
+	public void setMaplink(String maplink) {
+		this.maplink = maplink;
+	}
+	
+    
+	
+	
+	
+	
+}
